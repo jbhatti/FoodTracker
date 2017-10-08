a14a2da401bbd0d1f0ba7bc7cfd1ee7ca2a2b8a5
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-08 18:03:08 -0400

    add tableviewcell class

---
+++ MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jaison Bhatti on 2017-10-08.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
