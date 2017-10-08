430956a4756c32fc3cce35d0a06910e18239d94f
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-08 17:19:00 -0400

    implement the rating action

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -15,6 +15,12 @@ import UIKit
     private var ratingButtons = [UIButton]()
     
     var rating = 0
+    {
+        didSet
+        {
+            updateButtonSelectionStates()
+        }
+    }
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
     {
@@ -47,7 +53,24 @@ import UIKit
     //MARK: Button Action
     func ratingButtonTapped(button: UIButton)
     {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else
+        {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating
+        {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        }
+        else
+        {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
@@ -65,9 +88,15 @@ import UIKit
         
         // Load Button Images
         let bundle = Bundle(for: type(of: self))
-        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
-        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
-        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        let filledStar = UIImage(named: "filledStar",
+                                 in: bundle,
+                                 compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"emptyStar",
+                                in: bundle,
+                                compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar",
+                                      in: bundle,
+                                      compatibleWith: self.traitCollection)
         
         for _ in 0..<starCount
         {
@@ -96,8 +125,17 @@ import UIKit
             // Add the new button to the rating button array
             ratingButtons.append(button)
         }
+        
+        updateButtonSelectionStates()
     }
     
-    
+    private func updateButtonSelectionStates()
+    {
+        for (index, button) in ratingButtons.enumerated()
+        {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+    }
 
 }
