8679c08c626154c60cd57d9aca1d13ce0d911042
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-08 16:30:26 -0400

    add action to button

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,10 +13,17 @@ class RatingControl: UIStackView {
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
     }
     
     //MARK: Private Methods
@@ -29,6 +36,11 @@ class RatingControl: UIStackView {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self,
+                         action: #selector(RatingControl.ratingButtonTapped(button:)),
+                         for: .touchUpInside)
         
         // Add the button to the stack
         addArrangedSubview(button)
