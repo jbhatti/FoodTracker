c1c16b52a44fb354579cad9762f60c0e7b3cc114
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-08 16:02:29 -0400

    add UIStackView class

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jaison Bhatti on 2017-10-08.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
