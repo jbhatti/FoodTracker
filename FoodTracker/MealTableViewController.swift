56f3b14e56fbbbbcb798514c3635850f4a68befc
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-09 02:16:44 -0400

    create a file path to data

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -199,5 +199,13 @@ class MealTableViewController: UITableViewController
     {
         let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
         
+        if isSuccessfulSave
+        {
+            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        }
+        else
+        {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
     }
 }
