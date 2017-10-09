3ac44198c1baf826d646efbeca539dbe290a9ac4
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-09 02:08:38 -0400

    add types structure with properties

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -16,17 +16,28 @@ class Meal
     var photo: UIImage?
     var rating: Int
     
+    //MARK: Types
+    
+    struct PropertyKey
+    {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
     //MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int)
     {
         
         // The name must not be empty
-        guard !name.isEmpty else {
+        guard !name.isEmpty else
+        {
             return nil
         }
         
         // The rating must be between 0 and 5 inclusively
-        guard (rating >= 0) && (rating <= 5) else {
+        guard (rating >= 0) && (rating <= 5) else
+        {
             return nil
         }
         
